47270f44b875322ebc6dd30f16fffd9613a4e79d
JoaoPedroVisconti <visconti.joao@gmail.com>
2022-04-18 15:49:31 +0100

    Create and combine reducers

---
+++ frontend/src/reducers/blog_reducers.js
@@ -0,0 +1,44 @@
+import {
+  CREATE_BLOG,
+  DELETE_ALL_BLOGS,
+  DELETE_BLOGS,
+  FETCH_BLOGS,
+  UPDATE_BLOGS,
+} from '../actions/types'
+
+const initialState = []
+
+function blogReducer(blogs = initialState, action) {
+  const { type, payload } = action
+
+  switch (type) {
+    case CREATE_BLOG:
+      return [...blogs, payload]
+
+    case FETCH_BLOGS:
+      return payload
+
+    case UPDATE_BLOGS:
+      return blogs.map((blog) => {
+        if (blog.id == payload.id) {
+          return {
+            ...blog,
+            ...payload,
+          }
+        } else {
+          return blog
+        }
+      })
+
+    case DELETE_BLOGS:
+      return blogs.filter(({ id }) => id !== payload.id)
+
+    case DELETE_ALL_BLOGS:
+      return []
+
+    default:
+      return blogs
+  }
+}
+
+export default blogReducer
---
+++ frontend/src/reducers/index_reducers.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import blogReducer from './blog_reducers'
+
+export default combineReducers({
+  blogReducer,
+})
